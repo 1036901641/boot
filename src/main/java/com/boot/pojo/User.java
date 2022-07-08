de282f073b898716db2b6e5abe86f1a611449015
fengjin123 <1036901641@qq.com>
2022-07-08 09:39:55 +0800

    Third

--- src/main/java/com/boot/pojo/User.java
+++ src/main/java/com/boot/pojo/User.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @NotNull(message = "id 不能为空")
     private Integer id;
+
     private String name;
     private String pwd;
 }
